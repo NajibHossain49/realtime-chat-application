73c898de7e48da05fdee2c38b1ec356afc5def77
Najib Hossain <najibhossain1234@gmail.com>
2025-08-30 19:28:19 +0600

    Add basic Socket.IO chat server with room join, messaging, and typing events

--- server/index.js
+++ server/index.js
@@ -13,9 +13,29 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
-  
 });
 
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    const { room, message } = data;
+    socket.to(room).emit("receive_message", data);
+  });
+
+  socket.on("typing", ({ room, username }) => {
+    socket.to(room).emit("user_typing", username);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
 
 server.listen(3001, () => {
   console.log("Server is running on port 3001");
